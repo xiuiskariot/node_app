6ec391872ab42d7df595ebdd5f0e2f79ba3113d0
Ksusha <xiuschv@yandex.ru>
2023-04-14 17:41:56 +0600

    create user api

--- server/app.js
+++ server/app.js
@@ -1,8 +1,31 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const PORT = 5000;
 
+//наша база данных
+const users = [];
+
+app.use(cors());
+app.use(express.json());
+
+app.post("/user", (req, res) => {
+  const { name, password } = req.body;
+  let User = users.find((user) => user.name === name);
+
+  if (User) {
+    if (User.password === password) {
+      res.send("Добро пожаловать");
+    } else {
+      res.status(401).send("Неверный пароль");
+    }
+  } else {
+    users.push({ name, password });
+    res.status(201).send("Пользователь создан");
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`❤️❤️❤️Server has been started on port ${PORT}`);
-})
+});
